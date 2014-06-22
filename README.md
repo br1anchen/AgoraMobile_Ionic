222afdfa54da600ffd430500afe469e91f6148d3
Brian Chen <brianchen8990@gmail.com>
2014-06-22 22:19:45 +0200

    first init with basic code in ionic project

---
+++ README.md
@@ -0,0 +1,7 @@
+Agora Mobile 2.0
+
+UNINETT AS product
+
+Ionic Project
+
+Developed by Xiao Chen (brianchen8990@gmail.com)
---
+++ www/app/login/LoginCtrl.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('agoraMobile.controllers')
+  .controller('LoginCtrl',function($scope){
+
+})
